44422b26dad20c15c64a9c8a63d9d08df56584dd
Pedro Venzi <pedrovenzi@MacBook-Pro-de-Pedro.local>
2023-02-05 22:23:52 -0300

    setup inicial context

--- src/App.tsx
+++ src/App.tsx
@@ -6,21 +6,26 @@ import { PageButton } from './components/Buttons/PageButton';
 import { ImageContainer } from './components/Containers/ImageContainer';
 import { InputContainer } from './components/Containers/InputContainer';
 import { PageContainer } from './components/Containers/PageContainer';
+import { GlobalContext } from './components/contexts/useContext';
 import MainRoutes from './Routes';
+
+// const [copy, setCopy] = useState<string>(getCopy('User'))
 
 function App() {
   return (
     <BrowserRouter>
-      <PageContainer>
-        <PageButton text="batata"></PageButton>
-        <InputContainer />
-        <ImageContainer
-          photoUrl="https://images.ctfassets.net/3mv54pzvptwz/5eTv6hTyA1pqkFClYRn0qt/be626a573cbf1ee7e421b73f87ed6851/20221205_foto_GETTY_neymar_jr_jogo_brasil_x_coreia_copa_do_mundo__209_.jpg"
-          width="300px"
-          height="400px"
-        />
-      </PageContainer>
-      <MainRoutes/>
+      <GlobalContext.Provider value={{ copy, setCopy }}>
+        <PageContainer>
+          <PageButton text="batata"></PageButton>
+          <InputContainer />
+          <ImageContainer
+            photoUrl="https://images.ctfassets.net/3mv54pzvptwz/5eTv6hTyA1pqkFClYRn0qt/be626a573cbf1ee7e421b73f87ed6851/20221205_foto_GETTY_neymar_jr_jogo_brasil_x_coreia_copa_do_mundo__209_.jpg"
+            width="300px"
+            height="400px"
+          />
+        </PageContainer>
+      </GlobalContext.Provider>
+      <MainRoutes />
     </BrowserRouter>
   );
 }
