b338021b52ab7fc24aa7e4379b2b128fa0702a0d
Gabriel Cruz <gabrielcruzvazsantos@gmail.com>
2023-02-10 11:35:04 -0300

    [bug] router do not render pages

--- src/App.tsx
+++ src/App.tsx
@@ -16,17 +16,8 @@ function App() {
   return (
     <BrowserRouter>
       <GlobalContext.Provider value={{ copy, setCopy }}>
-        <PageContainer>
-          <PageButton text="batata"></PageButton>
-          <InputContainer />
-          <ImageContainer
-            photoUrl="https://images.ctfassets.net/3mv54pzvptwz/5eTv6hTyA1pqkFClYRn0qt/be626a573cbf1ee7e421b73f87ed6851/20221205_foto_GETTY_neymar_jr_jogo_brasil_x_coreia_copa_do_mundo__209_.jpg"
-            width="300px"
-            height="400px"
-          />
-        </PageContainer>
+        <MainRoutes />
       </GlobalContext.Provider>
-      <MainRoutes />
     </BrowserRouter>
   );
 }
--- src/Routes/index.tsx
+++ src/Routes/index.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 
+import { HomePage } from '../pages/HomePage';
+
 const MainRoutes = () => {
   return (
     <Routes>
-      <Route path="/" element={} />
+      <Route path="/" element={<HomePage />} />
     </Routes>
   );
 };
