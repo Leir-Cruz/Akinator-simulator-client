6b93262f5aba33528e20ac64ba0c6dec3a0a8109
Gabriel Cruz <gabrielcruzvazsantos@gmail.com>
2023-02-10 11:11:38 -0300

    [refactor] changing context file location

--- src/App.tsx
+++ src/App.tsx
@@ -7,7 +7,7 @@ import { PageButton } from './components/Buttons/PageButton';
 import { ImageContainer } from './components/Containers/ImageContainer';
 import { InputContainer } from './components/Containers/InputContainer';
 import { PageContainer } from './components/Containers/PageContainer';
-import { GlobalContext } from './components/contexts/useContext';
+import { GlobalContext } from './contexts/useContext';
 import MainRoutes from './Routes';
 
 const [copy, setCopy] = useState<string>('exemplo de contexto');
