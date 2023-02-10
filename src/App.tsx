7f183eb9930bfc7dc0570b61142ebe07dd279f9c
Gabriel Cruz <gabrielcruzvazsantos@gmail.com>
2023-02-10 11:39:48 -0300

    removing unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -3,10 +3,6 @@ import './App.css';
 import { useState } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
-import { PageButton } from './components/Buttons/PageButton';
-import { ImageContainer } from './components/Containers/ImageContainer';
-import { InputContainer } from './components/Containers/InputContainer';
-import { PageContainer } from './components/Containers/PageContainer';
 import { GlobalContext } from './contexts/useContext';
 import MainRoutes from './Routes';
 
