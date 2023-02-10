a63fe7dec4f8b9e5404e8d85fd6850dccbb60901
Gabriel Cruz <gabrielcruzvazsantos@gmail.com>
2023-02-10 15:39:26 -0300

    removing comments and unused imports

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,13 @@
 import './App.css';
 
-import { useState } from 'react';
 import { BrowserRouter } from 'react-router-dom';
 
-import { GlobalContext } from './contexts/useContext';
 import MainRoutes from './Routes';
-
-//const [copy, setCopy] = useState<string>('exemplo de contexto');
 
 function App() {
   return (
     <BrowserRouter>
-      {/* <GlobalContext.Provider value={{ copy, setCopy }}> */}
       <MainRoutes />
-      {/* </GlobalContext.Provider> */}
     </BrowserRouter>
   );
 }
