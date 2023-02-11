8efd5e4756f34b3c38d1a90202659b637d54b7cc
Gabriel Cruz <gabrielcruzvazsantos@gmail.com>
2023-02-11 15:20:57 -0300

    [feat] notFound page

--- src/Routes/index.tsx
+++ src/Routes/index.tsx
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom';
 
 import { CharacterPage } from '../pages/CharacterPage';
 import { HomePage } from '../pages/HomePage';
+import { NotFoundPage } from '../pages/NotFoundPage';
 import { QuestionCharacterPage } from '../pages/QuestionCharacterPage';
 import { QuestionPage } from '../pages/QuestionPage';
 
@@ -12,6 +13,7 @@ const MainRoutes = () => {
       <Route path="/question" element={<QuestionPage />} />
       <Route path="/questionCharacter" element={<QuestionCharacterPage />} />
       <Route path="/Character" element={<CharacterPage />} />
+      <Route path="/NotFound" element={<NotFoundPage />} />
     </Routes>
   );
 };
---
+++ src/pages/NotFoundPage/index.tsx
@@ -0,0 +1,29 @@
+import { keyframes, styled, Typography } from '@mui/material';
+import { useState } from 'react';
+
+import { PageButton } from '../../components/Buttons/PageButton';
+import { PageContainer } from '../../components/Containers/PageContainer';
+import { Input } from '../../components/Input/Input';
+
+export const NotFoundPage = () => {
+  const PageTitle = styled(Typography)(() => ({
+    fontSize: '64px',
+    width: '80%',
+    textAlign: 'center',
+    color: '#fff',
+    fontFamily: 'Zen Kurenaido',
+  }));
+
+  const [name, setName] = useState<string>('');
+  console.log(name);
+
+  return (
+    <PageContainer>
+      <PageTitle>
+        Seu Personagem não está no nosso Banco de Dados, pode nos dizer quem é?
+      </PageTitle>
+      <Input placeholder="Nome do Personagem" onChange={(e) => setName(e)} />
+      <PageButton text="Confirmar" width="25%" />
+    </PageContainer>
+  );
+};
