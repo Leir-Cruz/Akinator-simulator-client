026b03bead71ca528de75d6c17592536a3690ef3
Gabriel Cruz <gabrielcruzvazsantos@gmail.com>
2023-02-11 13:09:15 -0300

    [feat] Homepage

--- src/pages/HomePage/index.tsx
+++ src/pages/HomePage/index.tsx
@@ -1,15 +1,23 @@
-import { Typography } from '@mui/material';
+import { keyframes, styled, Typography } from '@mui/material';
 
 import { PageButton } from '../../components/Buttons/PageButton';
 import { PageContainer } from '../../components/Containers/PageContainer';
 
 export const HomePage = () => {
-  console.log('wtf');
+  //from { opacity: 0 }
+  const textFadeLoop = keyframes`
+  from { opacity: 0; transform: scale(0.8); }
+  to { opacity: 1; transform: scale(1); }
+  `;
+  const PageTitle = styled(Typography)(() => ({
+    fontSize: '128px',
+    color: '#fff',
+    fontFamily: 'Zen Kurenaido',
+    animation: `${textFadeLoop} 2s infinite alternate`,
+  }));
   return (
     <PageContainer>
-      <Typography sx={{ fontSize: '128px', color: '#fff', fontFamily: 'Zen Kurenaido' }}>
-        Akinator Simulator
-      </Typography>
+      <PageTitle>Akinator Simulator</PageTitle>
       <PageButton text="jogar" width="30%" />
     </PageContainer>
   );
