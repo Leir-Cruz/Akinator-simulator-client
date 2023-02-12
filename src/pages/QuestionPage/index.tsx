079be544fb005c7538ced696eda7a6a0c555cc82
Pedro Ximenes <pedroximenes76@gmail.com>
2023-02-12 19:24:21 -0300

    [feat]👽️💩 Estrutura inicial para integrar com o fastAPI
    Precisa arrumar a parte de enviar a resposta

--- src/pages/QuestionPage/index.tsx
+++ src/pages/QuestionPage/index.tsx
@@ -1,13 +1,34 @@
 import { Box } from '@mui/material';
+import { useEffect, useState } from 'react';
 
 import { PageButton } from '../../components/Buttons/PageButton';
 import { PageContainer } from '../../components/Containers/PageContainer';
 import { Question } from '../../components/Question';
+import { choice, useGlobalContext } from '../../contexts/useContext';
 
 export const QuestionPage = () => {
+  const { startNewGame, getNextQuestion, answerQuestion } = useGlobalContext();
+  const [title, setTitle] = useState<string>('');
+
+  const postAnswer = async (res: choice) => {
+    console.log('Teste');
+    console.log(res);
+    await answerQuestion(title, res);
+  };
+
+  const fetchNextQuestion = async () => {
+    await startNewGame();
+    const response = await getNextQuestion();
+    setTitle(response);
+  };
+
+  useEffect(() => {
+    fetchNextQuestion();
+  }, []);
+
   return (
     <PageContainer>
-      <Question question="Seu Personagem é fulano?" questionNumber={1} fontSize="48px" />
+      <Question question={title} questionNumber={1} fontSize="48px" />
       <Box
         sx={{
           display: 'flex',
@@ -18,9 +39,33 @@ export const QuestionPage = () => {
           marginBottom: '10px',
         }}
       >
-        <PageButton text="sim" width="250px" height="50px" color="#3BCEAC" />
-        <PageButton text="Não" width="250px" height="50px" color="#CE3B3B" />
-        <PageButton text="Não sei" width="250px" height="50px" color="#868E96" />
+        <PageButton
+          text="sim"
+          width="250px"
+          height="50px"
+          color="#3BCEAC"
+          onClick={() => {
+            postAnswer(1);
+          }}
+        />
+        <PageButton
+          text="Não"
+          width="250px"
+          height="50px"
+          color="#CE3B3B"
+          onClick={() => {
+            postAnswer(-1);
+          }}
+        />
+        <PageButton
+          text="Não sei"
+          width="250px"
+          height="50px"
+          color="#868E96"
+          onClick={() => {
+            postAnswer(0);
+          }}
+        />
       </Box>
     </PageContainer>
   );
